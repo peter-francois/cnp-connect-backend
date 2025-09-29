6c13f6ae2e3b5a115aefc5f59378d319ed85dcec
Peter Francois <peter.francois237@hotmail.com>
2025-09-29 16:37:56 +0200

    add class-validator
    Co-authored-by: Bipce <Bipce@users.noreply.github.com>
    Co-authored-by: NicolasPalay <NicolasPalay@users.noreply.github.com>

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,42 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from "class-validator";
 import { RoleEnum } from "@prisma/client";
+import { Transform } from "class-transformer";
 
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsStrongPassword({
+    minLength: 10,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
   firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
   lastName: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   hiredAt: Date;
-  avatarUrl?: string;
+
+  @IsNotEmpty()
+  @IsString()
   role: RoleEnum;
 }
