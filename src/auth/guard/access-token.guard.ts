07d1c335c04a49350afa752d11b50bb19e8bbb8c
Peter Francois <peter.francois237@hotmail.com>
2025-10-06 17:30:14 +0200

    Use custom exception in AccessTokenGuard

--- src/auth/guard/access-token.guard.ts
+++ src/auth/guard/access-token.guard.ts
@@ -1,12 +1,14 @@
 import {
   CanActivate,
   ExecutionContext,
+  HttpStatus,
   Injectable,
   UnauthorizedException,
 } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { PayloadInterface } from "../interfaces/payload.interface";
+import { CustomException } from "src/utils/custom-exception";
 
 @Injectable()
 export class AccesTokenGuard implements CanActivate {
@@ -16,7 +18,11 @@ export class AccesTokenGuard implements CanActivate {
     const request: Request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new CustomException(
+        "Unauthorized exception",
+        HttpStatus.UNAUTHORIZED,
+        "ATG-ca-1",
+      );
     }
     try {
       const payload: PayloadInterface = await this.jwtService.verifyAsync(
@@ -29,7 +35,11 @@ export class AccesTokenGuard implements CanActivate {
       // so that we can access it in our route handlers
       request["user"] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new CustomException(
+        "Unauthorized exception",
+        HttpStatus.UNAUTHORIZED,
+        "ATG-ca-2",
+      );
     }
     return true;
   }
