be386229c1c06fd77c1c9fc280cc08d408f751e3
Claire Royer <claireroyer57@gmail.com>
2025-10-31 16:13:51 +0100

    Move nats client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,11 @@ import { AuthModule } from "./auth/auth.module";
 import { EmailModule } from "./utils/mail/email.module";
 import { TokenService } from "./auth/token.service";
 import { ConfigModule } from "@nestjs/config";
-import { AppService } from "./app.service";
-import { AppController } from "./app.controller";
 import { TemplateModule } from "./template/template.module";
-import { NatsClientModule } from "./nats-client.module";
 
 @Module({
-  controllers: [AppController],
   imports: [
     PrismaModule,
-    NatsClientModule,
     UserModule,
     AuthModule,
     EmailModule,
@@ -23,6 +18,6 @@ import { NatsClientModule } from "./nats-client.module";
     }),
     TemplateModule,
   ],
-  providers: [TokenService, AppService],
+  providers: [TokenService],
 })
 export class AppModule {}
--- src/template/template.module.ts
+++ src/template/template.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { TemplateService } from "./template.service";
 import { TemplateController } from "./template.controller";
-import { NatsClientModule } from "src/nats-client.module";
+import { NatsClientModule } from "src/message_broker/nats-client.module";
 
 @Module({
   imports: [NatsClientModule],
   controllers: [TemplateController],
-  providers: [TemplateService],
 })
 export class TemplateModule {}
