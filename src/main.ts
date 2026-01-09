a277e24e9a0905f80d3b1c03b65223d272ac50c2
Peter Francois <peter.francois237@hotmail.com>
2026-01-09 14:20:26 +0100

    Clean up
    Co-authored-by: Baptiste Dinahet <Trekivoy@users.noreply.github.com>
    Co-authored-by: Bipce <Bipce@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe, Logger } from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
 import { PrismaExeptionFilter } from "./utils/filters/prisma-exeption.filter";
 import { CustomExceptionFilter } from "./utils/filters/custom-exception.filter";
 import cookieParser from "cookie-parser";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { isInDevMode } from "./utils/variables";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.set("trust proxy", 1);
+  // app.set("trust proxy", 1);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      disableErrorMessages:
-        process.env.NODE_ENV === "development" ? false : true,
+      disableErrorMessages: isInDevMode ? false : true,
     }),
   );
-  const logger = new Logger("Test");
-  logger.warn("process.env.FRONTEND_URL = " + process.env.FRONTEND_URL);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    // origin: "http://localhost:80",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
